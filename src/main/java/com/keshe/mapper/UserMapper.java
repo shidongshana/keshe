88a678ac8e3346087fe4367bc06018da7b913384
是东山啊 <12512711+sdsa123@user.noreply.gitee.com>
2024-12-16 18:04:22 +0800

    first

--- src/main/java/com/keshe/mapper/UserMapper.java
+++ src/main/java/com/keshe/mapper/UserMapper.java
@@ -1,4 +1,13 @@
 package com.keshe.mapper;
 
+import com.keshe.model.UserModel;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface UserMapper {
+@Select("SELECT * FROM user WHERE username = #{username} ")
+    UserModel getUserByUsername(String username);
+@Select("SELECT * FROM user WHERE id = #{id}")
+    UserModel getUserById(int id);
 }
--- src/main/java/com/keshe/service/UserService.java
+++ src/main/java/com/keshe/service/UserService.java
@@ -1,4 +1,32 @@
 package com.keshe.service;
 
-public class UserService {
+import com.keshe.mapper.UserMapper;
+import com.keshe.model.UserModel;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Resource
+    private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userMapper.getUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("登录失败，用户名或密码错误！");
+        }
+        return User.withUsername(user.getUsername()).password(passwordEncoder.encode(user.getPassword())).build();
+    }
+
+
 }
